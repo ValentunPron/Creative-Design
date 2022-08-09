1b88f49eb4558642349ab5107c9f2baa9fda795c
Valentyn <valentunpron@gmail.com>
2022-08-09 11:26:05 +0300

    stark work this main block

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 $(document).ready( () => {
+	// Burger
 	$('.header__burger').click((event) => {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock')
 	})
+	// Slide fone
+	$('.main__bg1').slick({
+		dots: true,
+		appendDots:$('.slider__dots'),
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 15000,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		arrows: false,
+		speed: 500,
+		fade: true,
+		dots: false,
+		cssEase: 'linear',
+	  });
+	$('.main__bg2').slick({
+		dots: true,
+		appendDots:$('.slider__dots'),
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 15000,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		arrows: false,
+		speed: 500,
+		fade: true,
+		dots: false,
+		cssEase: 'linear',
+	  });
 });
