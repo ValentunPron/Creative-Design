b914ab8c9d833b1fea1f3905f3b8c9dd7a7390b9
Valentyn <valentunpron@gmail.com>
2022-08-12 11:27:18 +0300

    new logo and active menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready( () => {
+	// Menu
+	$('.menu__item a').each((index, element) => {
+		$(`#${element.id}`).click(() => {
+			$('html, body').animate({
+				scrollTop: $(`.${element.id}`).offset().top
+			}, 500);
+			if ($('.header__burger, .header__menu').hasClass('active')) {
+				$('.header__burger, .header__menu').toggleClass('active');
+				$('body').toggleClass('lock')
+			}
+		})
+	}) 
 	// Burger
 	$('.header__burger').click((event) => {
 		$('.header__burger, .header__menu').toggleClass('active');
